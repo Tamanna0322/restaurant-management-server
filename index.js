56178a25dc86362a9902fd7145d73efb5a3b001e
Tamanna0322 <tamanna0322@gmail.com>
2024-05-14 00:16:23 +0600

    added search option

--- index.js
+++ index.js
@@ -100,13 +100,25 @@ async function run() {
       res.send(result);
     })
 
-    
+
     app.delete('/purchase/:id', async(req, res) =>{
       const id = req.params.id;
       const query = {_id: new ObjectId(id)}
       const result = await purchaseCollection.deleteOne(query);
       res.send(result);
     })
+
+    app.get('/search', async (req, res) => {
+      try {
+        const { searchQuery } = req.query; 
+        const filteredFoods = await restaurantCollection.find({
+          foodName: { $regex: searchQuery, $options: 'i' } 
+        }).toArray(); 
+        res.json(filteredFoods);
+      } catch (error) {
+        console.error('Error searching for foods:', error);
+      }
+    });
 
 
 
